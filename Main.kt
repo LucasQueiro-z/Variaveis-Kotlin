3d678c2b8af86a075ea543f1977760ce27665e52
Lucas <lucas.queiroz001@hotmail.com>
2022-06-05 13:47:37 -0300

    Váriaveis Kotlin

---
+++ Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    val ano: Int = 2023
+    val peso: Float = 75.3f
+    val PI: Double = 3.14159265359
+    val letra: Char = 'a'
+    val texto: String = "Olá Mundo!"
+
+    println(ano)
+    println(peso)
+    println(PI)
+    println(letra)
+    println(texto)
+
+}
